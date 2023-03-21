5e65cc7301150a884fce201b811001907942054c
Alisa Sobol <alisa.sobol@namecheap.com>
2023-03-21 22:04:53 +0100

    added search functionality and state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 import { CardList } from './components/CardList';
 import { Search }  from './components/Search';
 import { robots }  from './robots';
 
-const App = () => {
-  return (
-    <div className="App code bg-dark-green pa5">
-      <h1 className='f1 ttu white tracked-mega mt0'>Robofriends</h1>
-      <Search />
-      <CardList robots={ robots } />
-    </div>
-  ); 
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchField: '',
+    }
+  }
+
+  onSearchChange = event => {
+    this.setState({searchField: event.target.value})
+  }
+
+  render () {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+
+    return (
+      <div className="App code bg-dark-green pa5">
+        <h1 className='f1 ttu white tracked-mega mt0'>Robofriends</h1>
+        <Search searchCheange={this.onSearchChange}/>
+        <CardList robots={ filteredRobots } />
+      </div>
+    ); 
+  }
 }
 
 export default App;
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Search = () => {
+const Search = ({searchCheange, searchField}) => {
 	return (
 		<div className='pa2'>
-			<input className='w-50 input-reset pa2 ba bw1 bg-white b--green' type="search" placeholder='Search for a robofriend' />
+			<input onChange={searchCheange} className='w-50 input-reset pa2 ba bw1 bg-white b--green' type="search" placeholder='Search for a robofriend' />
 		</div>
 	);
 };
