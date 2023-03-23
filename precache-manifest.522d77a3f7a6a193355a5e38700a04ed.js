dac76dee5c0d80fccabec41ace560079ce6142de
Alisa Sobol <alisa.sobol@namecheap.com>
2023-03-23 12:24:12 +0100

    Updates

---
+++ precache-manifest.522d77a3f7a6a193355a5e38700a04ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1580c1bbbb922732e4",
+    "url": "/React-app/static/css/main.030e243c.chunk.css"
+  },
+  {
+    "revision": "3e1580c1bbbb922732e4",
+    "url": "/React-app/static/js/main.3e1580c1.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/React-app/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "44fd39c3f55afd7b2298",
+    "url": "/React-app/static/css/2.d08b5204.chunk.css"
+  },
+  {
+    "revision": "44fd39c3f55afd7b2298",
+    "url": "/React-app/static/js/2.44fd39c3.chunk.js"
+  },
+  {
+    "revision": "cf4fbd0bee51c872c44b",
+    "url": "/React-app/static/js/runtime~main.cf4fbd0b.js"
+  },
+  {
+    "revision": "e83dde94409d7dc0afe7b6f04d0d3d43",
+    "url": "/React-app/index.html"
+  }
+];
